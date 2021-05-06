747ed17cdec0e6bfab7360e035be72a630416ae3
Natsuko Higashikuni <nokusukun@yahoo.co.jp>
2021-05-07 07:55:54 +0800

    feat(kerfuffle_web): built kerfuffle_web client

---
+++ client/kerfuffle-web/build/static/js/react-syntax-highlighter_languages_refractor_ini.bf4dd91d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkerfuffle-web"]=this["webpackJsonpkerfuffle-web"]||[]).push([[60],{271:function(t,e,n){"use strict";function s(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=s,s.displayName="ini",s.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.bf4dd91d.chunk.js.map
